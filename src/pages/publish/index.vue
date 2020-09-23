31e0efd582699d81c25ba82e7c6a5ef6c5336c68
Belief <chailu@clickwifi.net>
2020-09-23 19:18:02 +0800

    update

--- src/pages/publish/index.vue
+++ src/pages/publish/index.vue
@@ -4,7 +4,14 @@
       <span class="titles">标题</span>
       <el-input class="title-input" v-model="input" placeholder="请输入标题"></el-input>
     </div>
-    <editor-bar v-model="detail" :isClear="isClear" @change="change"></editor-bar>  
+    <div class="publish-edit">
+      <span class="publish-content">内容</span>
+      <editor-bar class="publish-content-edit" v-model="detail" :isClear="isClear" @change="change"></editor-bar>  
+    </div>
+    <div class="submit">
+      <el-button type="primary" @click="submit">发布</el-button>
+      <el-button @click="cancle">取消</el-button>
+    </div>
   </div>
 </template>
 <script>
@@ -25,6 +32,18 @@ export default {
     change(val) {  
         console.log(val)  
     },
+    submit() {
+      this.$message({
+        message: '你不配,因为你傻',
+        type: 'warning'
+      });
+    },
+    cancle() {
+      this.$message({
+        message: '你还不配,因为你蠢',
+        type: 'warning'
+      });
+    }
   },
   components: {
     EditorBar
